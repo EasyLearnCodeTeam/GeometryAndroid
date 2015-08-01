577e1607af9791fe34cfba3e0d7b255eadac181f
Ngoc Diep <ngocdiep@ssf.vn>
2015-08-01 18:32:08 +0700

    Add toolbar

--- app/src/main/java/com/easyleancode/geometry/apps/MainActivity.java
+++ app/src/main/java/com/easyleancode/geometry/apps/MainActivity.java
@@ -1,30 +1,44 @@
 package com.easyleancode.geometry.apps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.easyleancode.geometry.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tool_bar)
+    Toolbar toolBar;
+    @Bind(R.id.image_help)
+    ImageView imageHelp;
+    @Bind(R.id.close_help)
+    ImageView closeHelp;
+    @Bind(R.id.recycle_shape)
+    RecyclerView recyclerShape;
+    @Bind(R.id.btn_add)
+    FloatingActionButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        setGeometry();
+        ButterKnife.bind(this);
+        setSupportActionBar(toolBar);
     }
 
     private void setGeometry() {
-        getSupportFragmentManager().beginTransaction().
-                replace(R.id.geometry_view, new DrawingFragment()).
-                disallowAddToBackStack().commit();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.geometry_view, new DrawingFragment())
+                .disallowAddToBackStack().commit();
     }
 
     @Override
