99b94a36017d12d5a1cfce608f4232f4d022ac34
Ngoc Diep <ngocdiep@ssf.vn>
2015-08-01 15:49:38 +0700

    Add GLSurface

--- app/src/main/java/com/easyleancode/geometry/apps/MainActivity.java
+++ app/src/main/java/com/easyleancode/geometry/apps/MainActivity.java
@@ -1,9 +1,11 @@
-package com.easyleancode.geometry;
+package com.easyleancode.geometry.apps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.easyleancode.geometry.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +16,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        setGeometry();
+    }
+
+    private void setGeometry() {
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.geometry_view, new DrawingFragment()).
+                disallowAddToBackStack().commit();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
