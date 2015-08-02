afa7e84c4a5533a85a5d2b419f4c773a0c98b26c
Ngoc Diep <ngocdiep@ssf.vn>
2015-08-02 13:19:30 +0700

    Move asset string to constant

---
+++ app/src/main/java/com/easyleancode/geometry/utils/Constant.java
@@ -0,0 +1,12 @@
+package com.easyleancode.geometry.utils;
+
+public class Constant {
+    public static final String PYRAMID = "Hình chóp";
+    public static final String PRISMATIC = "Hình lăng trụ";
+    public static final String PYRAMID_TOP = "Đỉnh";
+    public static final String PYRAMID_BOTTOM = "Đáy";
+
+    public static String[] gethapeTypes() {
+        return new String[]{PYRAMID, PRISMATIC};
+    }
+}
